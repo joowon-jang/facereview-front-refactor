56dd7fb5a90e6c6dee07732154f150859cc31634
inf0craw1 <inf0craw1@naver.com>
2023-10-13 18:49:00 +0900

    refactor: Edit router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthPage from "../pages/auth/AuthPage";
 import MainPage from "../pages/main/MainPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        {/* <Route path="/main" element={<MainPage />} /> */}
+        <Route path="/" element={<AuthPage />} />
+        <Route path="*" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
