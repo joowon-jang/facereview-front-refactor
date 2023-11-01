74eb528bb85e6f3cde24e35126176e8093b8ec90
inf0craw1 <inf0craw1@naver.com>
2023-11-02 02:25:39 +0900

    feat: Add button types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,12 +3,49 @@ import "./button.scss";
 
 type ButtonPropsType = {
   label: string;
+  type:
+    | "cta-full"
+    | "cta-fit"
+    | "cta-fit-secondary"
+    | "small"
+    | "small-outline"
+    | "extra-small"
+    | "with-keyboard"
+    | "cta-fixed"
+    | "cta-fixed-secondary";
   style?: React.CSSProperties;
+  isDisabled?: boolean;
+  onClick?: () => void;
 };
 
-const Button = ({ label, style }: ButtonPropsType): ReactElement => {
+const Button = ({
+  label,
+  type,
+  style,
+  isDisabled,
+  onClick,
+}: ButtonPropsType): ReactElement => {
+  const fontOfType = {
+    "cta-full": "font-label-large",
+    "cta-fit": "font-label-large",
+    "cta-fit-secondary": "font-label-large",
+    small: "font-label-medium",
+    "small-outline": "font-label-medium",
+    "extra-small": "font-label-small",
+    "with-keyboard": "font-label-large",
+    "cta-fixed": "font-label-large",
+    "cta-fixed-secondary": "font-label-large",
+  };
   return (
-    <button className="button font-label-large" style={style}>
+    <button
+      className={`button ${type} ${fontOfType[type]} ${
+        isDisabled ? "disabled" : null
+      }`}
+      style={style}
+      onClick={isDisabled ? () => {} : onClick}
+    >
+      <div className="dim"></div>
+
       {label}
     </button>
   );
