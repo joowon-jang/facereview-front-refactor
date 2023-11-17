2a7ef125b83faf6754499fb7aa1908a4a1b53998
inf0craw1 <inf0craw1@naver.com>
2023-11-17 18:56:30 +0900

    feat: Add access_token, refresh_token, setToken func

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -3,7 +3,16 @@ import { devtools, persist } from "zustand/middleware";
 
 interface AuthState {
   id: string;
+  access_token: string;
+  refresh_token: string;
   setId: (id: string) => void;
+  setToken: ({
+    access_token,
+    refresh_token,
+  }: {
+    access_token: string;
+    refresh_token: string;
+  }) => void;
 }
 
 export const useAuthStorage = create<AuthState>()(
@@ -11,7 +20,14 @@ export const useAuthStorage = create<AuthState>()(
     persist(
       (set) => ({
         id: "testid",
-        setId: (id) => set((state) => ({ id: state.id })),
+        access_token: "",
+        refresh_token: "",
+        setId: (id) => set((state) => ({ id: id })),
+        setToken: ({ access_token, refresh_token }) =>
+          set((state) => ({
+            access_token: access_token,
+            refresh_token: refresh_token,
+          })),
       }),
       {
         name: "auth-storage",
