d1dcc91d6b3026b8307d74c217419c34237e56de
inf0craw1 <inf0craw1@naver.com>
2023-11-17 18:55:44 +0900

    feat: Add Tutorial page

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -5,19 +5,23 @@ import MainPage from "../pages/main/MainPage";
 import ScreenContainer from "./ScreenContainer/ScreenContainer";
 import MyPage from "../pages/my/MyPage";
 import EditPage from "../pages/edit/EditPage";
+import TutorialPage from "pages/tutorial/TutorialPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<ScreenContainer />}>
+        <Route element={<ScreenContainer headerShown={true} />}>
           <Route path="/main" element={<MainPage />} />
           <Route path="/watch/:id" element={<WatchPage />} />
-          <Route path="*" element={<MainPage />} />
           <Route path="/my" element={<MyPage />} />
           <Route path="/edit" element={<EditPage />} />
+          <Route path="*" element={<MainPage />} />
         </Route>
-        <Route path="/auth" element={<AuthPage />} />
+        <Route element={<ScreenContainer headerShown={false} />}>
+          <Route path="/tutorial/:step" element={<TutorialPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
