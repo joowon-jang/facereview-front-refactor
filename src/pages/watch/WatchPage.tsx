d4f12e21999c138e0253cf5d62a6b37cff7e6733
inf0craw1 <inf0craw1@naver.com>
2023-11-19 19:30:58 +0900

    feat: Add comment container

--- src/pages/watch/WatchPage.tsx
+++ src/pages/watch/WatchPage.tsx
@@ -9,6 +9,16 @@ import { Options } from "youtube-player/dist/types";
 import "./watchpage.scss";
 import { socket } from "socket";
 import React from "react";
+import ProfileIcon from "components/ProfileIcon/ProfileIcon";
+import TextInput from "components/TextInput/TextInput";
+import UploadButton from "components/UploadButton/UploadButton";
+
+type CommentItemType = {
+  color: "default" | "happy" | "surprise" | "sad" | "angry";
+  nickname: string;
+  commentTime: string;
+  commentText: string;
+};
 
 const WatchPage = (): ReactElement => {
   const { id } = useParams();
@@ -66,7 +76,24 @@ const WatchPage = (): ReactElement => {
       markers: [6.308684233669997, 6.128119615854916],
     },
   ];
+  const commentData: CommentItemType[] = [
+    {
+      color: "default",
+      nickname: "닉네임뭐로하지",
+      commentTime: "12분 전",
+      commentText:
+        "나영석 피디님을 보며 정말 놀랐던 이유는 어떻게 이런 사람들을 모아서 이런 케미를 이뤄 내지? 라는 생각이 들었을 때였습니다. 또한 제가 처음 나피디님을 알았던 1박2일 이후로 많은 시간이 지났지만 그 시대에 머물지 않고 뿅뿅 지구오락실에서 MZ세대와 같이 할 때도 그 세대의 문화를 수용하고 배우시려는 태도가 정말 존경스러웠습니다. 앞으로도 재밌는 예능 많이 부탁드립니다.",
+    },
+    {
+      color: "sad",
+      nickname: "하하호호",
+      commentTime: "1일 전",
+      commentText:
+        "나영석피디를 서진씨가잘만난것이큰행운이라고본다 나영석피디화이팅이다 할머니팬",
+    },
+  ];
   const [camImgURL, setCamImgURL] = useState("");
+  const [comment, setComment] = useState("");
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current?.getScreenshot();
@@ -102,6 +129,32 @@ const WatchPage = (): ReactElement => {
     });
   }, []);
 
+  const CommentItem = ({
+    nickname,
+    commentTime,
+    commentText,
+    color = "default",
+  }: CommentItemType): ReactElement => {
+    return (
+      <div className="comment-item-container">
+        <ProfileIcon
+          type={"icon-small"}
+          color={color}
+          style={{ marginRight: "12px" }}
+        />
+        <div className="comment-text-wrapper">
+          <div className="comment-info-wrapper">
+            <div className="comment-nickname font-label-small">{nickname}</div>
+            <div className="comment-time-text font-label-small">
+              {commentTime}
+            </div>
+          </div>
+          <div className="comment-text font-body-medium">{commentText}</div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="watch-page-container">
       <div className="main-container">
@@ -125,6 +178,39 @@ const WatchPage = (): ReactElement => {
         />
         <div className="title font-title-medium">
           [문돼의 온도] EP.35 불여우와의 갈등
+        </div>
+        <div className="comment-container">
+          <div className="comment-input-container">
+            <ProfileIcon
+              type={"icon-medium"}
+              color={"default"}
+              style={{ marginRight: "12px" }}
+            />
+            <TextInput
+              inputType="underline"
+              value={comment}
+              onChange={setComment}
+              placeholder={"영상에 대한 의견을 남겨보아요"}
+            />
+            <UploadButton
+              onClick={() => {}}
+              style={{
+                marginLeft: "12px",
+                display: comment.length > 0 ? "block" : "none",
+              }}
+            />
+          </div>
+          <div className="comment-info-text font-title-small">댓글 231개</div>
+          <div className="comment-list-container">
+            {commentData.map((comment) => (
+              <CommentItem
+                nickname={comment.nickname}
+                commentTime={comment.commentTime}
+                commentText={comment.commentText}
+                color={comment.color}
+              />
+            ))}
+          </div>
         </div>
       </div>
       <div className="side-container">
