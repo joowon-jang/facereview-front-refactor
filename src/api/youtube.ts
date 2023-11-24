07397271f4f9fe8454312eea8c8db90ab27993cf
inf0craw1 <inf0craw1@naver.com>
2023-11-24 18:47:40 +0900

    feat: Add getVideoDetail api

--- src/api/youtube.ts
+++ src/api/youtube.ts
@@ -1,4 +1,4 @@
-import { VideoDataType } from "types";
+import { VideoDataType, VideoDetailType } from "types";
 import api from "./index";
 
 export const getAllVideo = async () => {
@@ -17,6 +17,18 @@ export const getPersonalRecommendedVideo = async () => {
   try {
     const url = "/home/user-customized-list";
     const { data } = await api.get<VideoDataType[]>(url);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getVideoDetail = async (props: { videoId: string }) => {
+  try {
+    const url = "/watch/main-youtube";
+    const { data } = await api.post<VideoDetailType>(url, props);
 
     return data;
   } catch (error) {
