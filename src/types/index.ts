7517e3732fedcc2ee5a79a3c9f3dd8cf3cf1c5b1
inf0craw1 <inf0craw1@naver.com>
2023-11-24 17:26:47 +0900

    feat: Add UserInfoType

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,15 @@
 export type EmotionType = "happy" | "surprise" | "sad" | "angry" | "neutral";
+export type UserInfoType = {
+  user_role: boolean;
+  user_name: string;
+  user_profile: number;
+  user_tutorial: number;
+  access_token: string;
+  refresh_token: string;
+};
+export type VideoDataType = {
+  youtube_url: string;
+  youtube_title: string;
+  youtube_most_emotion: EmotionType;
+  youtube_most_emotion_per: number;
+};
