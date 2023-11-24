92b3a76dad126c02db14451e67b9a8e87612aa30
inf0craw1 <inf0craw1@naver.com>
2023-11-24 20:59:20 +0900

    feat: Add watch api

---
+++ src/api/watch.ts
@@ -0,0 +1,41 @@
+import { VideoDetailType } from "types";
+import api from "./index";
+
+export const sendStartAnalysis = async (props: { youtube_index: number }) => {
+  try {
+    const url = "/watch/start-analysis";
+    const { data } = await api.post(url, props);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getVideoComments = async (props: { youtube_index: number }) => {
+  try {
+    const url = "/watch/comment-list";
+    const { data } = await api.post<VideoDetailType>(url, props);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const sendNewComment = async (props: {
+  comment_contents: string;
+  youtube_index: number;
+}) => {
+  try {
+    const url = "/watch/watch/add-comment";
+    const { data } = await api.post<VideoDetailType>(url, props);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
