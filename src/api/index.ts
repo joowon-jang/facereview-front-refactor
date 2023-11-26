09871aebd4d0171dac211029e0713b09de6106b2
inf0craw1 <inf0craw1@naver.com>
2023-11-27 01:11:38 +0900

    feat: Add youtubeApi

--- src/api/index.ts
+++ src/api/index.ts
@@ -30,4 +30,33 @@ api.interceptors.response.use(
   }
 );
 
+export const youtubeApi = axios.create({
+  baseURL: "https://www.googleapis.com/",
+  timeout: 30000,
+});
+
+youtubeApi.interceptors.request.use(
+  (config) => {
+    // console.log("🔮 [Req config]", config, "\n");
+    return config;
+  },
+  (error) => {
+    // console.log("🧨 [Req ERROR]", error, "\n");
+    return Promise.reject(error);
+  }
+);
+
+youtubeApi.interceptors.response.use(
+  (response) => {
+    // console.log("🔮 [Res]", response, "\n");
+    return response;
+  },
+  (error) => {
+    // console.log("🧨 [Res ERROR]", error, "\n");
+    if (error.status === 408) {
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
