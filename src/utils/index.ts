f88553c7ddd801f36168b5e5deabe34b6c685583
Joowon Jang <wndnjs5248@hanmail.net>
2023-11-26 18:35:35 +0900

    feat: Add mapEmotion util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,43 @@
+import { EmotionType } from "types/index";
+
 export const emojiOfEmotion = {
   happy: "😄",
   surprise: "😲",
   angry: "😠",
   sad: "😥",
   neutral: "😐",
+};
+
+export const mapEmotionToNumber = (prop: EmotionType) => {
+  switch (prop) {
+    case "neutral":
+      return 0;
+    case "happy":
+      return 1;
+    case "surprise":
+      return 2;
+    case "sad":
+      return 3;
+    case "angry":
+      return 4;
+    default:
+      return 0;
+  }
+};
+
+export const mapNumberToEmotion = (prop: number) => {
+  switch (prop) {
+    case 0:
+      return "neutral";
+    case 1:
+      return "happy";
+    case 2:
+      return "surprise";
+    case 3:
+      return "sad";
+    case 4:
+      return "angry";
+    default:
+      return "neutral";
+  }
 };
