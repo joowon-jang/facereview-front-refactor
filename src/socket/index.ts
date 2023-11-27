ae3f39e6ee19785bf2469d721fcad424a31f4d07
inf0craw1 <inf0craw1@naver.com>
2023-11-27 21:35:43 +0900

    feat: Add socket retry

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,6 +3,10 @@ import { io } from "socket.io-client";
 export const initSocketConnection = () => {
   const socket = io("http://34.64.238.124", {
     transports: ["websocket"],
+    reconnection: true,
+    reconnectionDelay: 100,
+    reconnectionDelayMax: 1000,
+    reconnectionAttempts: Infinity,
   });
 
   return socket;
