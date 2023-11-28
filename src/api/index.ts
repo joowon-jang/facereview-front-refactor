6022e1238b323eb6f870608d617676707a737239
inf0craw1 <inf0craw1@naver.com>
2023-11-28 15:17:39 +0900

    chore: Edit endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { ErrorResponseImpl } from "@remix-run/router/dist/utils";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.facereview.net",
+  baseURL: "http://api.facereview.net",
   timeout: 30000,
 });
 
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const initSocketConnection = () => {
-  const socket = io("https://api.facereview.net", {
+  const socket = io("http://api.facereview.net", {
     transports: ["websocket"],
     reconnection: true,
     reconnectionDelay: 100,
@@ -12,7 +12,7 @@ export const initSocketConnection = () => {
   return socket;
 };
 
-export const socket = io("https://api.facereview.net");
+export const socket = io("http://api.facereview.net");
 
 // // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
