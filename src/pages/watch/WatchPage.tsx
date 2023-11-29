818308025fca0709e9c59b8d4d7472d4ee46da7c
inf0craw1 <inf0craw1@naver.com>
2023-11-29 16:52:19 +0900

    feat: Add line chart

--- src/pages/watch/WatchPage.tsx
+++ src/pages/watch/WatchPage.tsx
@@ -46,6 +46,7 @@ import VideoItem from "components/VideoItem/VideoItem";
 import ModalDialog from "components/ModalDialog/ModalDialog";
 import safeImage from "assets/img/safeImage.png";
 import LikeButton from "components/LikeButton/LikeButton";
+import { ResponsiveLine } from "@nivo/line";
 
 const WatchPage = (): ReactElement => {
   const { v4: uuidv4 } = require("uuid");
@@ -557,6 +558,71 @@ const WatchPage = (): ReactElement => {
                 tooltip={() => <></>}
               />
             </div>
+          </div>
+          <div className="" style={{ width: "852px", height: "300px" }}>
+            <ResponsiveLine
+              data={videoGraphData}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: "point" }}
+              yScale={{
+                type: "linear",
+                min: "auto",
+                max: "auto",
+                stacked: true,
+                reverse: false,
+              }}
+              yFormat=" >-.2f"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "transportation",
+                legendOffset: 36,
+                legendPosition: "middle",
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "count",
+                legendOffset: -40,
+                legendPosition: "middle",
+              }}
+              pointSize={10}
+              pointColor={{ theme: "background" }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: "serieColor" }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: "bottom-right",
+                  direction: "column",
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: "left-to-right",
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: "circle",
+                  symbolBorderColor: "rgba(0, 0, 0, .5)",
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemBackground: "rgba(0, 0, 0, .03)",
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+            />
           </div>
           <div className="video-information-container">
             <div
